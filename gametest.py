14a1c46e4b4d8dbdf3a08947fd4752ed060aa267
Charles Ahrens Feldman <charlesahrensfeldman@gmail.com>
2017-02-11 22:54:20 -0600

    fixed roll numbers

--- gametest.py
+++ gametest.py
@@ -29,9 +29,7 @@ class Hex:
     def setCoords(self, coords):
         self.coords = coords
         
-    
-
-      
+        
 def generate_hex(edge_length, offset):
     """Generator for coordinates in a hexagon."""
     coords = []
@@ -45,20 +43,34 @@ def generate_hex(edge_length, offset):
     coords.append([offset])
     return coords
 
+
 def drawboard(canvas, tiles):
+
+    canvas.delete(all)
     length = 50
-    orig_x = 200
+    orig_x = 250
     orig_y = 200
-    xspace = 75
-    yspace = 45
-    vspace = 88
+    xspace = length*1.5+.5
+    yspace = length*0.866+.5
+    vspace = yspace*2+1
     x = orig_x
     y = orig_y
     i = 0
+
+    canvas.create_polygon([425,30,685,180,685,480,425,630,165,480,165,180], fill = 'cyan',stipple = 'gray50', width = 0)
     for h in tiles:
         coords = generate_hex(length,(x,y))
         color = 'red'
-        canvas.create_polygon(coords, tag=h.name ,fill = h.color,outline='black',stipple='gray75',activeoutline='red',activewidth=3)
+        canvas.create_polygon(coords,fill = 'white', width = 0)
+        canvas.create_polygon(coords, tag=h.name ,fill = h.color,outline=h.color,stipple='gray75',activeoutline='red',activewidth=3)
+        if h.name != 'desert':
+            textcolor = 'black'
+            if h.roll_num == 6 or h.roll_num == 8:
+                textcolor = 'red'
+            canvas.create_oval(x+10,y+29,x+40,y+59,fill = 'white', outline = 'white')
+            canvas.create_text(x+25,y+44,text = str(h.roll_num),fill = textcolor)
+        if h.name == 'desert':
+            canvas.create_oval(x+12,y+31,x+38,y+57,fill = 'grey', stipple = 'gray75', outline = 'gray')
 
         i += 1
         if i < 3 or (i > 3 and i <7) or (i>7 and i< 12) or (i>12 and i<16) or (i>16 and i<20):
@@ -76,57 +88,88 @@ def drawboard(canvas, tiles):
         elif i == 16:
             x = orig_x + 4*xspace
             y = orig_y
-
 def makenewboard():
+    ''' 
+           __
+        __/  \__
+     __/  \_7/  \__
+    /  \_3/  \12/  \
+    \_0/  \_8/  \16/
+    /  \_4/  \13/  \
+    \_1/  \_9/  \17/
+    /  \_5/  \14/  \
+    \_2/  \10/  \18/
+       \_6/  \15/ 
+          \11/ 
+    '''
+    #for 0 neighbors =                      i+1 i+3 i+4
+    #for 1 neighbors =                  i-1 i+1 i+3 i+4
+    #for 2 neighbors =                  i-1 i+1 i+3 i+4
+    #for 3 neighbors =              i-3     i+1     i+4 i+5
+    #for 4 neighbors =          i-4 i-3 i-1 i+1     i+4 i+5
+    #for 5 neighbors =          i-4 i-3 i-1 i+1     i+4 i+5
+    #for 6 neighbors =          i-4     i-1         i+4 i+5
+    #for 7 neighbors =          i-4         i+1         i+5
+    #for 8 neighbors =      i-5 i-4     i-1 i+1     i+4 i+5
+    #for 9 neighbors =      i-5 i-4     i-1 i+1     i+4 i+5
+    #for 10 neighbors =     i-5 i-4     i-1 i+1     i+4 i+5
+    #for 11 neighbors =     i-5         i-1         i+4
+    #for 12 neighbors =     i-5 i-4         i+1     i+4
+    #for 13 neighbors =     i-5 i-4     i-1 i+1 i+3 i+4
+    #for 14 neighbors =     i-5 i-4     i-1 i+1 i+3 i+4
+    #for 15 neighbors =     i-5 i-4     i-1     i+3
+    #for 16 neighbors =         i-4 i-3     i+1
+    #for 17 neighbors =         i-4 i-3 i-1 i+1
+    #for 18 neighbors =         i-4 i-3 i-1     
+
     random.seed()
     board = []
-    #for i in range(19):
-        #terr = ['field','forest','pasture','mountains','hills','desert']
+    nums = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+    random.shuffle(nums)
+    i = 0
     for j in range(4):
         terrain = 'field'
         name = 'field'+str(j)
-        tile = Hex(j,terrain,name)
+        tile = Hex(nums[i],terrain,name)
         board.append(tile)
 
         terrain = 'pasture'
         name = 'pasture'+str(j)
-        tile = Hex(j,terrain,name)
+        tile = Hex(nums[i+1],terrain,name)
         board.append(tile)
 
         terrain = 'forest'
         name = 'forest'+str(j)
-        tile = Hex(j,terrain,name)
+        tile = Hex(nums[i+2],terrain,name)
         board.append(tile)
+        i += 3
 
     for k in range(3):
         terrain = 'mountains'
         name = 'mountains'+str(k)
-        tile = Hex(k,terrain,name)
+        tile = Hex(nums[i],terrain,name)
         board.append(tile)
 
         terrain = 'hills'
         name = 'hills'+str(k)
-        tile = Hex(k,terrain,name)
+        tile = Hex(nums[i+1],terrain,name)
         board.append(tile)
+        i+=2
      
     terrain = 'desert'
     name = 'desert'
     tile = Hex(7,terrain,name)
     board.append(tile)
     random.shuffle(board)
-
+    
     return board
-
-def randomboard(canvas):
-    canvas.delete("all")
-    drawboard(canvas,makenewboard())
-    return 
-
 
 
 w = Canvas(root, width=800, height=800)
 menubar = Menu(root)
-menubar.add_command(label="new board", command=lambda: randomboard(w))
+hexlist = makenewboard()
+menubar.add_command(label="shuffle board",command = lambda: random.shuffle(hexlist))
+menubar.add_command(label="draw board", command=lambda: drawboard(w,hexlist))
 menubar.add_command(label="quit",command=lambda: root.destroy())
 root.config(menu=menubar)
 w.pack()
@@ -135,28 +178,17 @@ w.pack()
 def click(event):
     if w.find_withtag(CURRENT):
         ids = w.find_withtag(CURRENT)
-        w.itemconfig(CURRENT, width = 10)
+        w.itemconfig(CURRENT, width = 4)
         w.update_idletasks()
         w.after(200)
         w.itemconfig(CURRENT,width = 1)
-        print(w.itemcget(ids,'tag'))
-        #fill = w.itemcget(ids, "fill")
-   #     if fill == '#ffcc66':
-   #         terrain = 'field'
-   #     elif fill == '#009933':
-   #         terrain = 'forest':
-   #     elif fill == '#ccff66'
-   #         terrain = 'pasture':
-   #  self.stipple = 'gray25'
-   #elif terrain == 'mountains':
-   # self.color = '#808080'
-   #  self.stipple = 'gray75'
-   #elif terrain == 'hills':
-   #  self.color = '#cc6600'
-   #  self.stipple = 'gray25'
-   #elif terrain == 'desert':
-   #  self.color = '#ffffcc'
-   #  self.stipple = 'gray12'
+        tags = w.itemcget(ids,'tag')
+        name = tags.split(' ')[0]
+        currhex = filter(lambda x: x.name == name, hexlist)
+        if len(currhex) > 0:
+            print(currhex[0].name)
+            print(currhex[0].roll_num)
+
 
 w.bind("<Button-1>", click)
 
